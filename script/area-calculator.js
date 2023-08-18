74f9b9f638e5e8b2ff0269ddd24e1201665f49fd
tahmid530 <tahmidahmed530@gmail.com>
2023-08-19 00:46:52 +0600

    Pentagon Added

--- script/area-calculator.js
+++ script/area-calculator.js
@@ -19,12 +19,10 @@ function calculateRectangleArea() {
     const rectangleWidthField = document.getElementById('rectangle-width');
     const rectangleWidthFieldString = rectangleWidthField.value;
     const width = parseFloat(rectangleWidthFieldString);
-    console.log(width);
 
     const rectangleLengthField = document.getElementById('rectangle-length');
     const rectangleLengthFieldString = rectangleLengthField.value;
     const length = parseFloat(rectangleLengthFieldString);
-    console.log(length);
 
     const area = width * length;
 
@@ -38,12 +36,10 @@ function calculateParallelogramArea() {
     const parallelogramBaseField = document.getElementById('parallelogram-base');
     const parallelogramBaseFieldString = parallelogramBaseField.value;
     const base = parseFloat(parallelogramBaseFieldString);
-    console.log(base);
 
     const parallelogramHeightField = document.getElementById('parallelogram-height');
     const parallelogramHeightFieldString = parallelogramHeightField.value;
     const height = parseFloat(parallelogramHeightFieldString);
-    console.log(height);
 
     const area = base * height;
 
@@ -57,12 +53,10 @@ function calculateEllipseArea() {
     const ellipseMajorRadiusField = document.getElementById('ellipse-major-radius');
     const ellipseMajorRadiusFieldString = ellipseMajorRadiusField.value;
     const majorRadius = parseFloat(ellipseMajorRadiusFieldString);
-    console.log(majorRadius);
 
     const ellipseMinorRadiusField = document.getElementById('ellipse-minor-radius');
     const ellipseMinorRadiusFieldString = ellipseMinorRadiusField.value;
     const minorRadius = parseFloat(ellipseMinorRadiusFieldString);
-    console.log(minorRadius);
 
     const area = 3.1416 * majorRadius * minorRadius;
 
@@ -75,17 +69,32 @@ function calculateEllipseArea() {
 function calculateRhombusArea() {
     const rhombusDiagonals1Field = document.getElementById('rhombus-diagonals-1');
     const rhombusDiagonals1String = rhombusDiagonals1Field.value;
-    const rhombusDiagonals1 = parseFloat(rhombusDiagonals1String);
-    console.log(rhombusDiagonals1);
+    const Diagonals1 = parseFloat(rhombusDiagonals1String);
 
     const rhombusDiagonals2Field = document.getElementById('rhombus-diagonals-2');
     const rhombusDiagonals2String = rhombusDiagonals2Field.value;
-    const rhombusDiagonals2 = parseFloat(rhombusDiagonals2String);
-    console.log(rhombusDiagonals2);
+    const Diagonals2 = parseFloat(rhombusDiagonals2String);
 
-    const area = 0.5 * rhombusDiagonals1 * rhombusDiagonals2;
+    const area = 0.5 * Diagonals1 * Diagonals2;
 
     const rhombusAreaField = document.getElementById('rhombus-area');
     rhombusAreaField.innerText = area;
+
+}
+
+
+function calculatePentagonArea() {
+    const pentagonPerimeterField = document.getElementById('pentagon-perimeter');
+    const pentagonPerimeterFieldString = pentagonPerimeterField.value;
+    const perimeter = parseFloat(pentagonPerimeterFieldString);
+
+    const pentagonApothemField = document.getElementById('pentagon-apothem');
+    const pentagonApothemFieldString = pentagonApothemField.value;
+    const apothem = parseFloat(pentagonApothemFieldString);
+
+    const area = 0.5 * perimeter * apothem;
+
+    const ellipseAreaField = document.getElementById('ellipse-area');
+    ellipseAreaField.innerText = area;
 
 }
